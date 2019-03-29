e6281fab82ac7af1aa977b2a81ef3a6d6ee1cb8b
Gil16 <33487553+Gil16@users.noreply.github.com>
2019-03-29 12:18:16 +0300

    Initial commit

---
+++ Assets/Scripts/BallManager.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class BallManager : MonoBehaviour {
+
+    public static BallManager Instance;
+
+    public SteamVR_TrackedObject trackedObj;
+
+    private GameObject currentBall;
+
+    public GameObject ballPrefab;
+
+
+    private static int counter = 0;
+
+    private static bool throwing = false;
+
+    private static Vector3 pointA = Vector3.positiveInfinity;
+
+    private static float lastVelocity;
+
+    private const int LIMIT = 5000;
+
+    private const int SPEED_MULTIPLIER = 1;
+
+    private const float FPS = 0.012f;
+
+    private const float MIN_SPEED = 400f;   //  ////////////////////////////////////////   NOT     FINISHED     !!!!!
+
+    private static Vector3[] points = new Vector3[LIMIT];
+
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        attachBall();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        Vector3 pointB;
+        float currentVelocity;
+        if (throwing == false)
+        {
+            // Initialazing coords
+            if (pointA == Vector3.positiveInfinity)
+            {
+                pointA = trackedObj.transform.position;
+                pointB = trackedObj.transform.position;
+                lastVelocity = 0;
+            } else {
+                pointB = trackedObj.transform.position;
+            }
+
+            currentVelocity = Vector3.Distance(pointB, pointA) / FPS;
+            // Starting throwing, adding the points to the array
+            if (currentVelocity >= MIN_SPEED)
+            {
+                throwing = true;
+                points[counter++] = pointA;
+                points[counter++] = pointB;
+            }
+
+        } else {
+            
+            // Counter reached the points array limit, we copy 3 coords and continuing
+            if (counter == LIMIT)
+            {
+                points[0] = points[counter-2];
+                points[1] = points[counter - 1];
+                points[2] = points[counter];
+                counter = 2;
+            }
+            pointB = trackedObj.transform.position;
+            points[counter++] = pointB;
+            currentVelocity = Vector3.Distance(points[counter], points[counter-1]) / FPS;
+            lastVelocity = Vector3.Distance(points[counter - 1], points[counter - 2]) / FPS;
+            if (currentVelocity > lastVelocity)
+            {
+                // calculate the vector of throwing, throwing speed, make it rigid, usen gravity, counter=0
+                Vector3 throwingDirection = points[counter] - points[counter - 1];
+                float throwingVelocity = currentVelocity * SPEED_MULTIPLIER;
+
+                currentBall.AddComponent<Rigidbody>();
+                currentBall.GetComponent<Rigidbody>().AddRelativeForce(throwingDirection * throwingVelocity);
+                currentBall.GetComponent<Rigidbody>().useGravity = true;
+
+                currentBall.transform.parent = null;
+                currentBall = null;
+                counter = 0;
+            }
+            
+        }
+
+        lastVelocity = currentVelocity;
+        pointA = pointB;
+    }
+   
+
+
+        public void attachBall()
+    {
+        if (currentBall == null)
+        {
+            counter++;
+            currentBall = Instantiate(ballPrefab);
+            currentBall.transform.parent = trackedObj.transform;
+            currentBall.transform.position = trackedObj.transform.position + new Vector3(0.037f, -0.081f, 0.15f);
+            currentBall.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+
+
+
+
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Destroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+
+}
