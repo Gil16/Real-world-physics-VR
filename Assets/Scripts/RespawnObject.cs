d0a30d2be329c81c281db674e51b768e88acbf55
Gil16 <33487553+Gil16@users.noreply.github.com>
2019-03-30 18:04:18 +0300

    respawn objects

---
+++ Assets/Scripts/RespawnObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnObject : MonoBehaviour {
+
+    public GameObject objectPrefab;
+
+    private GameObject currentObject;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        createObject();
+        if (currentObject.transform.position.z < -30f)
+        {
+            currentObject.transform.parent = null;
+            Destroy(currentObject);
+        }
+    }
+
+    private void createObject()
+    {
+        if (currentObject == null)
+        {
+            currentObject = Instantiate(objectPrefab);
+            currentObject.transform.parent = objectPrefab.transform;
+            currentObject.transform.position = new Vector3(-0.05f, 2.98f, 30f);
+            currentObject.transform.localRotation = Quaternion.identity;
+        }
+    }
+
+}
